f32d1d435704099e48e3348748dd2589488b2497
park_so_young <soyoung931014@gmail.com>
2022-01-16 02:34:04 +0900

    Practice_Memo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-// props 결국 Btn()함수를 불러서 banana라는 인자를 아래와 같이 보내는것과 같다.
-// Btn({banana: "save changes"}) Btn은 함수
-// 어떤 prop이든 Btn 컴포넌트에 보내면 그것들은 Btn 함수의 첫 번째 argument(인자)속으로 들어갈거야
-// props는 첫번째 유일한 인자이다. props옆에 다른 인자 들어올 수 없음 
-// btn에 함수에 props를 넘겨준 후, console.log(props)를 하고 확인을 해보자.
-// 2개의 props를 볼 수 있는데 이말은 2번 렌더링이 되었다는것이다. 
-// 모든 props(properties)들은 props = {} props라는 객체 안에 모조리 들어간다.
-// shortcut으로서 props대신 {banana}로 대체되어 사용한다. banana말고 다른 props 가져오는경우 {banana, big}이런식으로 한다.
-function Btn(props) {
-  console.log(props) //{banana: 'Continue'} {banana: 'Save Changes'}
-  console.log(props.big) // 2번 렌더링이니까 true, undefined나옴
-  return <button
+// 컴포넌트가 상태를 바꾼다면 다시 render한다.
+// 따라서 9열에 console.log를 보면 처음 Save Changes was rendere, Continue was rendered
+// 버튼 누르고 다시한번 찍으면 Revert Changes was rendered, Continue was rendered
+// state변화했지만.. 모든것이render이 되었다. 우리는 어느 한 메소드를 통해 상태변경이 일어나지 않는 <Btn text="Continue"/>컴포넌트는
+// 렌더링이 되지 않게 만드는 방법을 사용해본다. const MemorizedBtn = React.memo(Btn)
+// 부모가 state를 변경하면(App) 아래에 있는것들이(자식)(모든게) 다시 그려진다. -> 단점: 추후에 어플리케이션이 느려지는 원인이 될 수도 있다.
+
+// 근데 우리는 컴포넌트를 다시 그릴지 말지를 결정할 수 있어(porp이 변경되지 않는 한에서)
+ import { useState } from "react"
+
+function Btn({text, changeValue}) {
+ console.log(text, "was rendered") // 콘솔로그 들어가서 꼭 확인해보자
+  return (
+          <button
+            onClick ={changeValue}
             style ={{ // 이 style들을 재 사용하고 싶어
               backgroundColor: "tomato",
               color: "white",
               padding: "10px 20px",
               border: 0,
-              borderRadius: 10,
-              fontSize: props.big ? 18: 9
+              borderRadius: 10,             
             }}
-  >
-    {props.banana} {/* // 같은 banana이지만 각각의<Btn/> 컴포넌트에 따라 달리 설정이 되는걸 볼 수 있다. */}
-  </button>
+            >
+              {text} 
+            </button>
+            )
 } 
 
 
 function App() {
+ const [value, setValue] = useState("Save Changes")
+ const changeValue = () => setValue("Revert Changes")
   return ( 
   <div>
-    <Btn banana="Save Changes" big ="true"/>
-    <Btn banana="Continue"/>
+    <Btn text={value} changeValue = {changeValue}/>
+    <Btn text="Continue"/>
   </div>
 
   ) 
